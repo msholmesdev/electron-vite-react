6e21d043bd5813ed6f6d7a82bd272c1c94bf9651
msholmesdev <msholmes.dev@gmail.com>
2025-05-22 15:07:07 -0400

    added temp spinning to load

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,7 @@
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="size-4 m-8 outline outline-dashed rounded-full border-blue-400 animate-spin outline-offset-[34px]" />
+  );
 };
 
 export { Loading };
